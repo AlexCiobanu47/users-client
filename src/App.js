5dff4229e221af019e2f9a9bd52d98e2e8f90bbb
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-03-21 10:45:29 +0200

    Added AddUser page and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
+import AddUser from "./users/AddUser";
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Home></Home>
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
